30c748a91c480448751e380e8666bd2fcef5830a
Borys <dev.bosk@gmail.com>
2024-08-16 23:19:38 +0200

    linked-list: push

---
+++ DataStructures/linked-list.js
@@ -0,0 +1,33 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+
+  push(val) {
+    let node = new Node(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+let list = new SinglyLinkedList();
+list.push("Hi");
+list.push("There");
+
+console.log(list);
