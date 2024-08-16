79322bc879589890499dffb5f9409814d15216ef
Borys <dev.bosk@gmail.com>
2024-08-16 23:47:06 +0200

    linked-list: pop

--- DataStructures/linked-list.js
+++ DataStructures/linked-list.js
@@ -24,10 +24,36 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
 
 let list = new SinglyLinkedList();
 list.push("Hi");
 list.push("There");
+list.push("Guys");
+list.push("!");
+
+console.log(list.pop());
+console.log(list.pop());
 
 console.log(list);
