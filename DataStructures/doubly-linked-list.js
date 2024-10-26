7289dce70908575fa11b78d73fd7a00df68183cf
Borys <dev.bosk@gmail.com>
2024-10-26 23:09:18 +0200

    doubly linked list: push method

---
+++ DataStructures/doubly-linked-list.js
@@ -0,0 +1,28 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+
+  push(val) {
+    const node = new Node(val);
+    if (this.length == 0) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+    }
+    this.tail = node;
+    this.length += 1;
+    return this;
+  }
+}
