3cb0fbca91ef9c9c135b4f9095453b82a7729a86
Borys <dev.bosk@gmail.com>
2024-10-28 11:59:23 +0100

    doubly linked list: shift, get, set, print

--- DataStructures/doubly-linked-list.js
+++ DataStructures/doubly-linked-list.js
@@ -40,4 +40,84 @@ class DoublyLinkedList {
     this.length--;
     return oldTail;
   }
+
+  shift() {
+    if (this.length === 0) return undefined;
+    const oldHead = this.head;
+    if (this.length === 1) {
+      this.tail = null;
+      this.head = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
+
+  unshift(val) {
+    const newNode = new Node(val);
+    if (this.length === 0) {
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+    }
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    const reverseDirection = index > this.length / 2;
+    let tempIndex, current;
+
+    if (reverseDirection) {
+      tempIndex = this.length - 1;
+      current = this.tail;
+      while (index !== tempIndex) {
+        current = current.prev;
+        tempIndex--;
+      }
+    } else {
+      tempIndex = 0;
+      current = this.head;
+      while (index !== tempIndex) {
+        current = current.next;
+        tempIndex++;
+      }
+    }
+
+    return current;
+  }
+
+  set(index, val) {
+    let node = this.get(index);
+    if (node) {
+      node.val = val;
+      return true;
+    }
+
+    return false;
+  }
+
+  print() {
+    let tempNode = this.head;
+    while (tempNode) {
+      console.log(tempNode.val);
+      tempNode = tempNode.next;
+    }
+  }
 }
+
+const d = new DoublyLinkedList();
+d.push(1);
+d.push(12);
+d.push(123);
+d.push(1234);
+d.push(1235);
+d.set(3, 33);
+d.print();
+console.log(d.get(3));
